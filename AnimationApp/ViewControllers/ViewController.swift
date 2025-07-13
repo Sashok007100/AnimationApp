45c79b727d35d9cfc191b5961aa3800c1067c8fd
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-13 16:21:12 +0300

    Была создана переменная, для связи с моделью Animation.

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -13,6 +13,9 @@ final class ViewController: UIViewController {
     @IBOutlet var animationView: SpringView!
     @IBOutlet var startAnimationButton: UIButton!
     @IBOutlet var animationDescriptionLabel: UILabel!
+    
+    private var animation = Animation.randomAnimation
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
