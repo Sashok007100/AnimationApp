7cc7eab5e0c1e8ae7e83d14a7fe5babe08288ae4
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-13 16:26:28 +0300

    Была реализована логика кнопки для начала анимации.

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -24,4 +24,17 @@ final class ViewController: UIViewController {
         animationDescriptionLabel.text = animation.description
     }
 
+    @IBAction func startAnimationTapped() {
+        animationDescriptionLabel.text = animation.description
+        
+        animationView.animation = animation.preset
+        animationView.curve = animation.curve
+        animationView.force = animation.force
+        animationView.duration = animation.duration
+        animationView.delay = animation.delay
+        animationView.animate()
+        
+        animation = .randomAnimation
+        startAnimationButton.setTitle("Run \(animation.preset)", for: .normal)
+    }
 }
